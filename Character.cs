74e1b4a77f049b9676f9d759b14d87eb89ea850c
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-05-22 19:36:10 -0700

    Update

---
+++ Character.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog122_S24_L12_ListView
+{
+    public class Character
+    {
+        // fields
+        string _playerName;
+        string _characterName;
+
+        public int CharacterLevel { get; private set; }
+
+        int _intelligence;
+        int _constitution;
+
+        int _proficiency;
+
+
+        public Character(string playerName)
+        {
+            _playerName = playerName;
+        }
+
+        public Character(int proficiency)
+        {
+            Proficiency = proficiency;
+        }
+
+        public Character(string playerName, string characterName)
+        {
+            _playerName = playerName;
+            _characterName = characterName;
+        }
+
+        public Character(string playerName, string characterName, int intelligence, int constitution)
+        {
+            _playerName = playerName;
+            _characterName = characterName;
+            _intelligence = intelligence;
+            _constitution = constitution;
+        }
+
+        public string PlayerName { get => _playerName; set => _playerName = value; }
+        public string CharacterName { get => _characterName; set => _characterName = value; }
+        public int Intelligence { get => _intelligence; set => _intelligence = value; }
+        public int Constitution { get => _constitution; set => _constitution = value; }
+
+        public int Hacking { get => AbilityModifer(_intelligence); }
+        public int WillPower { get => AbilityModifer(_constitution); }
+        public int Proficiency
+        {
+            get => _proficiency;
+            set
+            {
+                if (value > 0)
+                {
+                    _proficiency = value;
+                }
+                else
+                {
+                    throw new Exception("Enter a postive");
+                }
+
+            }
+        }
+
+        public int AbilityModifer(int stat)
+        {
+            return (stat - 10) / 2;
+        }
+
+        // Stat - 10 / 2 
+        // 8 - 10 / 2
+
+        // 8 - 9 -1
+
+        // 10 - 10 / 2
+
+        // 10 - 11 0
+
+        // 12 - 10 / 2
+        // 12 - 13 1
+
+    } // class
+     
+} // namespace
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Numerics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,41 @@ namespace Prog122_S24_L12_ListView
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Character> characterNames = new List<Character>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+
+            // We've changed our List<string> to List<Character> and populated it with 3 new characters
+            // We have only added our player names so far
+            characterNames.Add(new Character("Will", "McDurken", 14, 9));
+            characterNames.Add(new Character("Rafael", "Ragavan Quicksmith", 18, 16));
+            characterNames.Add(new Character("Charles", "Conan", 10, 17));
+
+            lvCharacters.ItemsSource = characterNames;
+
+        } // MainWindow()
+
+        private void btnDisplayInfo_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedIndex = lvCharacters.SelectedIndex;
+
+            if(lvCharacters.SelectedIndex != -1)
+            {
+                //Character selectedCharacter = (Character)lvCharacters.SelectedValue;
+                Character selectedCharacter = lvCharacters.SelectedValue as Character;
+
+                MessageBox.Show(selectedCharacter.PlayerName);
+
+            }
+            else
+            {
+                MessageBox.Show("Select a row from the list view");
+            }
+
         }
-    }
-}
+    } // class
+
+} // namespace
